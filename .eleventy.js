29e807e6f6317080d0f941fbbd5096631d55a40b
Eamonn de Leastar <edeleastar@wit.ie>
2022-03-07 13:24:40 +0000

    install eleventy plugin

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,7 @@
+const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPassthroughCopy("images");  
+  eleventyConfig.addPlugin(eleventyNavigationPlugin);
 };
